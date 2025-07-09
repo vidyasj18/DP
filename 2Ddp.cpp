08e72a391c2ae6f5f3d57aeb3b0a11529141874d
vidya s j <vidyajamkhandi1@gmail.com>
2025-07-10 00:55:42 +0530

    2Ddp qs

--- 2Ddp.cpp
+++ 2Ddp.cpp
@@ -161,3 +161,46 @@ public:
     }
 };
 
+
+// QS - 4 : Minimum path sum - leetcode
+// from (0,0) to (m-1,n-1) in a (m*n) grid
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(i==0 && j==0) {
+                    dp[0][0] = grid[0][0];
+                }
+
+                else {
+                    // if we assign 0 then 0 is minimum than grid[i][j]
+                    //  When either i == 0 or j == oneof                valuesleft or up will remain 0, which incorrectly contributes to min(left, up).
+
+// So for example, when i=0, up remains 0, and left is a valid sum. Then min(left, up) will wrongly select 0, which is not correct.
+
+
+                    int left = INT_MAX;
+                    int up = INT_MAX;
+                    if(i>0) {
+                        up = grid[i][j] + dp[i-1][j];
+                    }
+
+                    if(j>0) {
+                        left = grid[i][j] + dp[i][j-1];
+                    }
+
+                    dp[i][j] = min(left,up);
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
+
