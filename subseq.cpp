4a343fdf85880a9660e350b05647818aa6c6616d
vidya s j <vidyajamkhandi1@gmail.com>
2025-07-22 22:05:54 +0530

    subseq qs

--- subseq.cpp
+++ subseq.cpp
@@ -190,3 +190,59 @@ public:
         return cnt;
     }
 };
+
+
+// QS - 5 : minimum coins or change coin - Leetcode
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        // coins size(), target
+        vector<vector<int>> dp(n,vector<int>(amount+1));
+
+        // base case 
+        // when the 0th index is divisible by the target
+        // then no. of coins needed will be equal to coins[0]/target
+        // assign the no. of coins needed in dp array
+
+        for(int t = 0; t<=amount; t++) {
+            if(t%coins[0]==0) {
+                dp[0][t] = t/coins[0];
+            }
+
+            else {
+                dp[0][t] = 1e9;
+            }
+        }
+
+        // recursive relation
+
+        for(int ind = 1; ind<n; ind++) {
+            for(int tar = 0; tar<=amount; tar++) {
+                // if we dont take, ind will get decreased
+                int nottake = 0 + dp[ind-1][tar];
+                int take = INT_MAX;
+                
+                if(tar>=coins[ind]) {
+                    // if we take we will see, can we take
+                    // the same coin thats why ind remains same
+                    take = 1 + dp[ind][tar-coins[ind]];
+                }
+
+                // assign which is min in take and nottake
+                dp[ind][tar] = min(nottake,take);
+            }       
+        }
+
+        // ans lies in the last ind of dp table
+        int ans = dp[n-1][amount];
+        // if ans is INT_MAX or not defined then return -1
+        if(ans>=1e9) {
+            return -1;
+        }
+
+        // return ans if ans is defined
+        return ans;
+    }
+};
