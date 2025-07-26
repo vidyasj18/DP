a263def30eb445344bac3216d888af84d923c5b2
vidya s j <vidyajamkhandi1@gmail.com>
2025-07-26 22:49:13 +0530

    str qs

--- strdp.cpp
+++ strdp.cpp
@@ -161,3 +161,44 @@ public:
         return dp[n][n];
     }
 };
+
+
+// qs - 4 : delete operation for 2 strings
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        
+        vector<vector<int>> dp(m+1,(vector<int>(n+1,0)));
+
+        // base case
+        for(int i = 0; i<m; i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int i = 0; i<n; i++) {
+            dp[0][i] = 0;
+        }
+
+        int ans = 0;
+
+        for(int ind1 = 1; ind1<=m; ind1++) {
+            for(int ind2 = 1; ind2<=n; ind2++) {
+                if(word1[ind1-1]==word2[ind2-1]) {
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                }
+
+                else {
+                    dp[ind1][ind2] = max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                }
+            }
+        }
+
+        ans = dp[m][n];
+        // no. of elements which are not similar in both the words 
+        int ans1 = m - ans + n - ans;
+        return ans1;
+    }
+};
